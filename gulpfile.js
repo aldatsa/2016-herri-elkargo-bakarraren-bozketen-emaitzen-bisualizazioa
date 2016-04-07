38ce6b15d11a8a532e6a5f54ca4938327b270a1f
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-04-07 22:15:35 +0200

    browser-sync instalatu eta probatu

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+
+var reload = browserSync.reload;
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch(['index.html', 'css/**/*.css', 'js/**/*.js'], {cwd: './'}, reload);
+});
 
 gulp.task('default', function() {
     // place code for your default task here
