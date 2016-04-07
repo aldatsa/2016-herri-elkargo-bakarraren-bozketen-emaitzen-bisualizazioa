c3945330e58e2b239fee686b8b2333f4f580f870
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-04-07 23:11:56 +0200

    Beharrezko css eta js fitxategiak public karpetara eramateko gulp ataza sortu

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,29 @@ var reload = browserSync.reload;
 gulp.task('serve', function() {
     browserSync({
         server: {
-            baseDir: './'
+            baseDir: 'public/'
         }
     });
 
-    gulp.watch(['index.html', 'css/**/*.css', 'js/**/*.js', 'csv/*.csv'], {cwd: './'}, reload);
+    gulp.watch(['index.html', 'css/**/*.css', 'js/**/*.js', 'csv/*.csv'], {cwd: 'public/'}, reload);
+});
+
+gulp.task('kopiatuPublikora', function() {
+
+    console.log("Beharrezko JS eta CSS fitxategiak bower_components karpetatik public karpetara kopiatzen...");
+
+    // JS script fitxategiak kopiatu.
+    gulp.src(['bower_components/c3/c3.min.js',
+              'bower_components/d3/d3.min.js',
+              'bower_components/index.js',
+              'bower_components/topojson/topojson.js'], {
+                  base: 'bower_components'
+    }).pipe(gulp.dest('public/js'));
+
+    // CSS fitxategiak kopiatu.
+    gulp.src(['bower_components/c3/c3.min.css'], {
+                  base: 'bower_components'
+    }).pipe(gulp.dest('public/css'));
 });
 
 gulp.task('default', function() {
