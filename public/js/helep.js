a0ab1ae215f881ff75c174e87b88f717a52042d3
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-07-05 22:02:58 +0200

    Beste 2 legendak kendu

--- public/js/helep.js
+++ public/js/helep.js
@@ -967,26 +967,6 @@
                     return kolorea;
                 });
 
-                legend.selectAll('text')
-                    .data([0, 1, 2]) // 3 kolore daude legendan, baina aukerak.koloreak objektu bat da, ez array bat.
-                    .enter()
-                    .append("text")
-                    .attr("x", 525)
-                    .attr("y", function(d, i){ return 325 + i * 25;})
-            	    .text(function(d, i) {
-
-                        var legenda = "";
-
-                        if (i === 0) {
-                            legenda = "Alde";
-                        } else if (i === 1) {
-                            legenda = "Aurka";
-                        } else if (i === 2) {
-                            legenda = "Ez dago bozketara deitua *";
-                        }
-
-                        return legenda;
-                    });
                 legend.selectAll("foreignObject")
                     .data([0, 1, 2]) // 3 elementu daude legendan, baina aukerak.koloreak objektu bat da, ez array bat.
                     .enter()
